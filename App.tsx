a67685602149304da9270541798f46fef9df996a
Alvaro Tabata <elclubdelasmenteson21@gmail.com>
2026-05-19 23:54:08 -0400

    test: render only plain View with zero custom imports

--- App.tsx
+++ App.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
-import { StatusBar, LogBox, View } from 'react-native';
-
-import { AuthProvider } from '@context/AuthContext';
-import AppNavigator from '@navigation/AppNavigator';
-import { COLORS } from '@constants/theme';
-
-LogBox.ignoreLogs([
-  'Non-serializable values were found in the navigation state',
-  'ViewPropTypes will be removed',
-]);
+import { View } from 'react-native';
 
 const App: React.FC = () => {
-  return (
-    <View style={{ flex: 1, backgroundColor: COLORS.background }}>
-      <View style={{ flex: 1 }}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor={COLORS.background}
-          translucent={false}
-        />
-        <AuthProvider>
-          <AppNavigator />
-        </AuthProvider>
-      </View>
-    </View>
-  );
+  return <View style={{ flex: 1, backgroundColor: '#FF6B00' }} />;
 };
 
 export default App;
