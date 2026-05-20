2a9f3e1c03a38e303a91c252e61fd8e2fbb967ed
Alvaro Tabata <elclubdelasmenteson21@gmail.com>
2026-05-19 23:35:03 -0400

    test: remove react-native-gesture-handler import from index.js

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StatusBar, View } from 'react-native';
+import { StatusBar, LogBox, View } from 'react-native';
 
+import { AuthProvider } from '@context/AuthContext';
 import AppNavigator from '@navigation/AppNavigator';
 import { COLORS } from '@constants/theme';
+
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+  'ViewPropTypes will be removed',
+]);
 
 const App: React.FC = () => {
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.background }}>
-      <StatusBar barStyle="light-content" backgroundColor={COLORS.background} translucent={false} />
-      <AppNavigator />
+      <View style={{ flex: 1 }}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={COLORS.background}
+          translucent={false}
+        />
+        <AuthProvider>
+          <AppNavigator />
+        </AuthProvider>
+      </View>
     </View>
   );
 };
