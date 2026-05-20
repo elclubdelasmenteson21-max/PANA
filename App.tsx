ad1ff9a7e62537b8a6d3ad750e46835acbdc4575
Alvaro Tabata <elclubdelasmenteson21@gmail.com>
2026-05-19 22:39:53 -0400

    test: disable Hermes and reduce to minimal View to isolate Yoga crash

--- App.tsx
+++ App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { StatusBar, LogBox, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
-import { AuthProvider } from '@context/AuthContext';
 import AppNavigator from '@navigation/AppNavigator';
 import { COLORS } from '@constants/theme';
-
-LogBox.ignoreLogs([
-  'Non-serializable values were found in the navigation state',
-  'ViewPropTypes will be removed',
-]);
 
 const App: React.FC = () => {
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.background }}>
-      <View style={{ flex: 1 }}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor={COLORS.background}
-          translucent={false}
-        />
-        <AuthProvider>
-          <AppNavigator />
-        </AuthProvider>
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor={COLORS.background} translucent={false} />
+      <AppNavigator />
     </View>
   );
 };
